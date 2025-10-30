b5eb4a5156db48d96eb419d944b4364348f563ab
jeel150 <shahjeel865@gmail.com>
2025-10-30 16:41:42 +0530

    Fix Vite MIME type issue with correct Vercel config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this is set to root
+  base: './', // Ensure this is set to root
   build: {
     outDir: 'dist',
   }
